3286329b1b948265a13f8cc759dbe601b73e88f5
Dzmitry Ratskevich <ratskevichdg@gmail.com>
2021-03-06 01:20:33 +0300

    functions which add info about RAM and swap added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psutil as ps
 import math
 
+
 # TEMPLATES = {
 #     'cpu': {
 #         'times': None
@@ -18,15 +19,38 @@ def get_cpu_percent():
 
         return res
 
-    
+def get_memory():
+    res = {}
+    memory_info = ps.virtual_memory()
+    res['total'] = memory_info.total / (1024**3)
+    res['used'] = memory_info.used / (1024**3)
+    # посмотри тут подробнее, почему у тебя использованной больше, чем на самом
+    # деле если сравнить с оригинальным htop
+    res['used_bar'] = math.ceil(res['used'] * 100 / res['total'])
+
+    return res
+
+def get_swap_memory():
+    res = {}
+    swap_memory_info = ps.swap_memory()
+    res['total'] = swap_memory_info.total / (1024**3)
+    res['used'] = swap_memory_info.used / (1024**3)
+    # посмотри тут подробнее, почему у тебя использованной больше, чем на самом
+    # деле если сравнить с оригинальным htop
+    res['used_bar'] = math.ceil(res['used'] * 100 / res['total'])
+
+    return res
 
 def show(**kwargs):
     for kwarg in kwargs:
         print(kwarg)
 
 def main():
-    print(get_cpu_percent())
+    #print(get_cpu_percent())
+    print(get_memory())
+    print(get_swap_memory())
     # pass
+
 
 if __name__ == '__main__':
     main()
